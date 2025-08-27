ebda70f5855f8c7a98ff6ecf6db2e28778a4ed0f
GabeCasciano <gabecasciano@gmail.com>
2025-08-27 17:55:33 -0400

    refactoring and fixing bugs

---
+++ include/Settings_Mapping.h
@@ -0,0 +1,43 @@
+#ifndef MAPPING_H_
+#define MAPPING_H_
+
+#include "SQLiteWrapper/include/SQL_Value.h"
+
+#define MAX_KEY_LENGTH (32)
+
+struct Mapping_t {
+  unsigned short count;
+  char **keys;
+  SqlValue *values;
+
+  // default
+  Mapping_t() = default;
+  // constructor
+  Mapping_t(unsigned short count) : count(count) {
+    keys = new char *[count];
+    values = new SqlValue[count];
+  }
+
+  // destructor
+  ~Mapping_t() { destroy(); }
+
+  // copy
+  // copy assignment
+  //
+  // move
+  // move assignment
+
+private:
+  void destroy() {
+    if (keys)
+      delete[] keys;
+    if (values)
+      delete[] values;
+  }
+
+  void copy_from(const Mapping_t &o) {}
+
+  void move_from(Mapping_t &&o) {}
+};
+
+#endif // !MAPPING_H_
